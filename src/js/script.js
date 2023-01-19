dd56f375e29314f64b751d923325e7907efadaa1
Anna Urbanik <urbanik.ania@gmail.com>
2023-01-19 23:54:17 +0100

    Add double click for favourites

--- src/js/script.js
+++ src/js/script.js
@@ -6,22 +6,50 @@ const select = {
         bookList: '.books-list',
         bookImage: '.book__image',
     },
+    class: {
+        favorite: 'favorite',
+    },
 };
 
 const templates = {
     menuBooks: Handlebars.compile(document.querySelector(select.templateOf.bookTemplate).innerHTML),
 };
 
+const favoriteBooks = [];
+
 function render() {
     for (const book of dataSource.books) {
         // generate HTML based on the template and on a given book's data
-        const generatedHTML = templates.menuBooks(book); 
+        const generatedHTML = templates.menuBooks(book);
         // generate DOM element
-        const bookHTML= utils.createDOMFromHTML(generatedHTML);
+        const bookHTML = utils.createDOMFromHTML(generatedHTML);
+        //console.log('html', bookHTML)
         // generated DOM element join as a new DOM child to the list .books-list
-        const listOfBooks= document.querySelector(select.templateOf.bookList);
+        const listOfBooks = document.querySelector(select.templateOf.bookList);
+        //console.log(listOfBooks);
         listOfBooks.appendChild(bookHTML);
     }
 }
 
+function initActions() {
+    const books = document.querySelectorAll(select.templateOf.bookImage);
+
+    for (let book of books) {
+        //event listener
+        book.addEventListener('dblclick', function (event) {
+            //preventDefault
+            event.preventDefault();
+            //add class 'favorite'
+            book.classList.add(select.class.favorite);
+            //book's id from data-id
+            const bookId = book.getAttribute('data-id');
+            console.log(book);
+            // add id to favoriteBooks
+            favoriteBooks.push(bookId);
+        })
+    }
+}
+
 render();
+
+initActions();

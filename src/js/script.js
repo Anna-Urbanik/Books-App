f061a288df23a54db2563e7e3203fcb9c0f5ba21
Anna Urbanik <urbanik.ania@gmail.com>
2023-01-20 21:53:11 +0100

    Continue with event delegation

--- src/js/script.js
+++ src/js/script.js
@@ -1,62 +1,61 @@
 'use strict';
 
 const select = {
-  templateOf: {
-    bookTemplate: '#template-book',
-    bookList: '.books-list',
-    bookImage: '.book__image',
-  },
-  class: {
-    favorite: 'favorite',
-  },
+    templateOf: {
+        bookTemplate: '#template-book',
+        bookList: '.books-list',
+        bookImage: '.book__image',
+    },
+    class: {
+        favorite: 'favorite',
+    },
 };
 
 const templates = {
-  menuBooks: Handlebars.compile(document.querySelector(select.templateOf.bookTemplate).innerHTML),
+    menuBooks: Handlebars.compile(document.querySelector(select.templateOf.bookTemplate).innerHTML),
 };
 
 const favoriteBooks = [];
 
 function render() {
-  for (const book of dataSource.books) {
-    // generate HTML based on the template and on a given book's data
-    const generatedHTML = templates.menuBooks(book);
-    // generate DOM element
-    const bookHTML = utils.createDOMFromHTML(generatedHTML);
-    //console.log('html', bookHTML)
-    // generated DOM element join as a new DOM child to the list .books-list
-    const listOfBooks = document.querySelector(select.templateOf.bookList);
-    //console.log(listOfBooks);
-    listOfBooks.appendChild(bookHTML);
-  }
+    for (const book of dataSource.books) {
+        // generate HTML based on the template and on a given book's data
+        const generatedHTML = templates.menuBooks(book);
+        // generate DOM element
+        const bookHTML = utils.createDOMFromHTML(generatedHTML);
+        //console.log('html', bookHTML)
+        // generated DOM element join as a new DOM child to the list .books-list
+        const listOfBooks = document.querySelector(select.templateOf.bookList);
+        //console.log(listOfBooks);
+        listOfBooks.appendChild(bookHTML);
+    }
 }
 
 function initActions() {
-  const books = document.querySelectorAll(select.templateOf.bookImage);
-
-  for (let book of books) {
+    //const books = document.querySelectorAll(select.templateOf.bookImage);
+    const books = document.querySelector(select.templateOf.bookList);
     //event listener
-    book.addEventListener('dblclick', function (event) {
-      console.log(book);
-      //preventDefault
-      event.preventDefault();
-      //check whether event.target.offsetParent owns class .book__image
-      if(event.target.offsetParent.classList.contains('.book__image')) { 
-      //get book's id from data-id
-      const bookId = event.target.offsetParent.getAttribute('data-id');
-      //console.log(bookId);
-      // add id to favoriteBooks
-      if (!favoriteBooks.includes(bookId)) {
-        book.classList.add(select.class.favorite);
-        favoriteBooks.push(bookId);
-      } else {
-        book.classList.remove(select.class.favorite);
-        const index = favoriteBooks.indexOf(bookId);
-        favoriteBooks.splice(index, 1);
-      }
-    }
+    books.addEventListener('dblclick', function (event) {
+        //preventDefault
+        event.preventDefault();
+        //check whether event.target.offsetParent owns class .book__image
+        if (event.target.offsetParent.classList.contains('.book__image')) {
+            //get book's id from data-id
+            const bookId = event.target.offsetParent.getAttribute('data-id');
+            //const book = ????
+            //console.log(bookId);
+            // add id to favoriteBooks
+            if (!favoriteBooks.includes(bookId)) {
+                book.classList.add(select.class.favorite);
+                favoriteBooks.push(bookId);
+            } else {
+                book.classList.remove(select.class.favorite);
+                const index = favoriteBooks.indexOf(bookId);
+                favoriteBooks.splice(index, 1);
+            }
+        }
     })
-  }
+
 }
 
 
